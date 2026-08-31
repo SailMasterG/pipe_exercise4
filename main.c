db2560b9c5692916cdf6b26c71ca0f5717c82c5d
SailMaster91 <sail91@gmail.com>
2026-08-31 14:54:19 +0200

    [format] del duplicate code

--- main.c
+++ main.c
@@ -30,15 +30,17 @@ int main(void)
 {
 	t_cmd cmd[3];
 	pid_t pid[3];
+	int n;
 	int fd[2];
 	int old_fd;
 	int i;
 
+	n = 3;
 	old_fd = -1;
 	i = 0;
 	while(i < 3)
 	{
-		if(i < 3 - 1)
+		if(i < n -1)
 			pipe(fd);
 		pid[i] = fork();
 		if(pid[i] == 0)
@@ -48,38 +50,26 @@ int main(void)
 				dup2(old_fd, STDIN_FILENO);
 				close(old_fd);
 			}
-			if(i < 2)
+			if(i < n - 1)
 			{
 				dup2(fd[1], STDOUT_FILENO);
 				close(fd[1]);
 				close(fd[0]);
 			}
 			if(i == 0)
-			{
 				parssing_cmd(&cmd[i], "/usr/bin/ls", (char*[]){"ls",NULL} );
-				execve(cmd[i].path, cmd[i].args, NULL);
-				perror("No se pudo ejecutar comando CMD1");
-				exit(127);
-			}
 			else if (i == 1)
-			{
 				parssing_cmd(&cmd[i], "/usr/bin/grep", (char*[]){"grep","a", NULL} );
-				execve(cmd[i].path, cmd[i].args, NULL);
-				perror("No se pudo ejecutar comando CMD2");
-				exit(127);
-			}
 			else if (i == 2)
-			{
 				parssing_cmd(&cmd[i], "/usr/bin/cat", (char*[]){"cat",NULL} );
-				execve(cmd[i].path, cmd[i].args, NULL);
-				perror("No se pudo ejecutar comando CMD3");
-				exit(127);
-			}
+			execve(cmd[i].path, cmd[i].args, NULL);
+			perror("No se pudo ejecutar comando CMD3");
+			exit(127);
 		}
 		if(old_fd != -1)
 			close(old_fd);
 		old_fd = fd[0];
-		if(i < 2)
+		if(i < n -1)
 			close(fd[1]);
 		i++;
 	}
